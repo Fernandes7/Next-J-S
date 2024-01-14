a268a9f3e2e0f7072cd235ad672504724399b62f
FernoLinux <fernooxplozd@gmail.com>
2024-01-14 20:45:06 +0530

    its my commit

--- src/app/new/dashboard/template.tsx
+++
@@ -1,7 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-    console.log("It is template");
-    return <div>
-        <h2>the Template</h2>
-        {children}
-        </div>
-  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import styles from './page.module.css'
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>GTA 5</h1>
-      </div>
+    <main>
+      <h2 className={styles.startingh2}>Starting Page of Pocket PG</h2>
     </main>
   )
 }
