54528f62e2c974f87ee2f6348206952e51837e0f
FernoLinux <fernooxplozd@gmail.com>
2024-01-20 21:26:57 +0530

    player 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main>
-      <h2 className={styles.startingh2}>Starting Page of Pocket PG</h2>
+      <h2 className={styles.startingh2}>Starting Page Player Twos of Pocket PG</h2>
       <Homelogin />
     </main>
   )
