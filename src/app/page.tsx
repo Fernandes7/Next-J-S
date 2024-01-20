793bde432725e1c4c853d8a00c8b32e2377d4953
FernoLinux <fernooxplozd@gmail.com>
2024-01-20 22:09:10 +0530

    new line in player1 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h2 className={styles.startingh2}>Starting Page By Player 1 of Pocket PG </h2>
+      <h2>Hello moto</h2>
       <Homelogin />
     </main>
   )
